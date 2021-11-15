aeb8c96e9f8cdfa27ae27fca84963ec9976010a8
soymendo <maximendoza92@hotmail.com.ar>
2021-11-15 17:13:54 -0300

    partial Sofware

--- Teletrabajo/Teletrabajo/ViewModels/FormularioVM.cs
+++ Teletrabajo/Teletrabajo/ViewModels/FormularioVM.cs
@@ -11,5 +11,15 @@ namespace Teletrabajo.ViewModels
         public List<Trabajador> Trabajadores { get; set; }
         public RepresentanteLegal RepresentanteLegal { get; set; }
         public List<Empresa> Empresas { get; set; }
+
+
+        public bool EncuestaCalidad { get; set; }
+        public bool DispositivoDeControl { get; set; }
+        public bool FijacionPlazos { get; set; }
+        public bool EvaluacionLaboral { get; set; }
+        public bool EstablecimientoIndicadores { get; set; }
+        public bool SoftwareParaMonitorear { get; set; }
+
+        public string Otros { get; set; }
     }
 }
